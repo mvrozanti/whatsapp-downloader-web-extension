4bda8189a3e7dae3fb56ca8fadad4677e825a8ae
Marcelo <mvrozanti@hotmail.com>
2020-12-27 00:56:41 -0300

    now able to get some datetime and sender information per message (needs work on audio sent, video received and some others)

--- test.js
+++ test.js
@@ -1,15 +1,46 @@
 document.getElementById("downloadButton").addEventListener('click', () => {
-    console.log("Popup DOM fully loaded and parsed");
     function modifyDOM() {
         function getElementByXpath(path) {
-          return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        }
-        var nome = document.getElementsByTagName('header')[1].getElementsByTagName('span')[1].textContent;
-        // console.log('v1');
-        console.log('nome: '+ nome);
+            return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        };
+        var conversationTitle = document.getElementsByTagName('header')[1].getElementsByTagName('span')[1].textContent;
+        console.log('conversationTitle: ' + conversationTitle);
+
         var mainConversationArea = document.querySelector('#main');
-        var mainConversationDiv = document.querySelector('#main').getElementsByTagName('div')[2];
-        var messageList = getElementByXpath('/html/body/div[1]/div/div/div[4]/div/div[3]/div/div/div[3]')
+        var mainConversationDiv = mainConversationArea.getElementsByTagName('div')[2];
+        var messageList = getElementByXpath('/html/body/div[1]/div/div/div[4]/div/div[3]/div/div/div[3]');
+        var messageListDivs = messageList.getElementsByTagName('div');
+
+        var messages = [];
+        Array.prototype.forEach.call(messageListDivs, function(el) {
+            if(el.hasAttribute('class')){
+                var elClass = el.getAttribute('class');
+                var arr = /message-(\w{2,3})/g.exec(elClass);
+                if(arr != null){
+                    var divsForGettingToDateTimeSender = el.getElementsByTagName('div');
+                    var datetimeSender = null;
+                    Array.prototype.forEach.call(divsForGettingToDateTimeSender, function(el) {
+                        // msg de texto:
+                        if(el.hasAttribute('data-pre-plain-text'))
+                            datetimeSender = el.getAttribute('data-pre-plain-text');
+                    });
+                    var sender = null;
+                    var datetime = null;
+                    if(datetimeSender == null){
+                        // msg com mídia
+                        Array.prototype.forEach.call(el.getElementsByTagName('span'), function(el) {
+                            if(el.hasAttribute('aria-label'))
+                                sender = el.getAttribute('aria-label');
+                            if(el.hasAttribute('dir') && el.getAttribute('dir') == 'auto')
+                                datetime = '[' + el.textContent + ', __/__/____] ';
+                        });
+                        datetimeSender = datetime + sender
+                    }
+
+                    console.log(datetimeSender)
+                }
+            }
+        });
 
         return document.body.innerHTML;
     }
